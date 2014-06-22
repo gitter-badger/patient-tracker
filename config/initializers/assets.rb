5bf8fd03f01b38fd2d7dd76a6b9c48cb57173b8a
sealocal <local.mat@gmail.com>
2014-06-21 21:12:41 -0700

    add fonts to precompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,16 @@
-Rails.application.config.assets.precompile += %w( gibson.css hours_selector.css jquery_mobile.css jquery_mobile.js jquery_ui.js)
+Rails.application.config.assets.precompile += %w(
+  gibson.css
+  hours_selector.css
+  jquery_mobile.css
+  jquery_mobile.js
+  jquery_ui.js
+  gibsonlight-webfont.eot
+  gibsonlight-webfont.ttf
+  gibsonlight-webfont.woff
+  gibsonregular-webfont.eot
+  gibsonregular-webfont.ttf
+  gibsonregular-webfont.woff
+  gibsonsemibold-webfont.eot
+  gibsonsemibold-webfont.ttf
+  gibsonsemibold-webfont.woff
+)
