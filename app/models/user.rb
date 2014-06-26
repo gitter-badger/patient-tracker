469603f38bd8bfb250eae8e8b01296aa2ab3c219
sealocal <local.mat@gmail.com>
2014-06-25 23:28:00 -0700

    concatenate names and assign default role

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,14 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :patients
+
+  before_save :set_name, :set_role
+
+  def set_name
+    self.name = first_name + ' ' + last_name
+  end
+
+  def set_role
+    self.role = 'Resident'
+  end
 end
