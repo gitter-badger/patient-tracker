4f7f4127d815651c2643aaeca69807ea2d6793f2
sealocal <local.mat@gmail.com>
2014-06-26 17:05:08 -0700

    add pundit patient policy

--- app/policies/patient_policy.rb
+++ app/policies/patient_policy.rb
@@ -10,6 +10,30 @@ class PatientPolicy < ApplicationPolicy
     user.name == patient.user.name || user.role == 'Admin'
   end
 
+  def show?
+    index?
+  end
+
+  def new?
+    create?
+  end
+
+  def create?
+    user.role == 'Resident'
+  end
+
+  def edit?
+    update?
+  end
+
+  def update?
+    user.role == 'Resident' && user.name == patient.user.name
+  end
+
+  def destroy?
+    create?
+  end
+
   class Scope < Struct.new(:user, :scope)
     def resolve
       scope
